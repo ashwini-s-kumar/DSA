8cc6832cc2792ff2bebd01c8fd6ed3f5fdb79a28
Ashwini S Kumar <ashwini.s@healsoftware.ai>
2022-05-01 00:48:18 +0530

    test

--- src/main/java/com/dsa/trees/SizeBT.java
+++ src/main/java/com/dsa/trees/SizeBT.java
@@ -6,6 +6,7 @@ package com.dsa.trees;
 public class SizeBT {
     public static int sizeBT(Node root)
     {
+
         if(root == null)
             return 0;
         return 1+ sizeBT(root.left) + sizeBT(root.right);
