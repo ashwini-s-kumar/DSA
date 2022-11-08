1703f567de534b3eed0e3e6dacd62e42abdae65d
Ashwini S Kumar <ashwiniskumar64@gmail.com>
2022-11-08 21:18:47 +0530

    DSA

--- src/main/java/com/dsa/graph/dfsproblems/CycleDetectionDirectedGraph.java
+++ src/main/java/com/dsa/graph/dfsproblems/CycleDetectionDirectedGraph.java
@@ -2,27 +2,28 @@ package com.dsa.graph.dfsproblems;
 
 import com.dsa.graph.AdjacentListGraph;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Queue;
 
-public class CycleDetection {
-    static boolean detectCycle(ArrayList<ArrayList<Integer>> adj, int source, boolean [] visited, int parent)
+public class CycleDetectionDirectedGraph {
+    static boolean detectCycle(ArrayList<ArrayList<Integer>> adj, boolean [] visited, boolean [] recursionStack, int source)
     {
+
+        recursionStack[source] = true;
         visited[source] = true;
         for(int x: adj.get(source))
         {
-            if(visited[x] == false) {
-                if(detectCycle(adj, x, visited,source) == true) {
+            if(visited[x] == false)
+            {
+                if(detectCycle(adj, visited, recursionStack, x))
                     return true;
-                }
             }
-            else if(x != parent) {
+            else if(recursionStack[x] == true)
                 return true;
-            }
         }
+        recursionStack[source] = false;
         return false;
     }
+
     public static void main(String [] arrs)
     {
         int  v = 7;
@@ -38,15 +39,16 @@ public class CycleDetection {
         AdjacentListGraph.insertEdge(adj, 2, 3);
         AdjacentListGraph.insertEdge(adj, 4, 5);
         AdjacentListGraph.insertEdge(adj, 4, 6);
-        AdjacentListGraph.insertEdge(adj, 5, 6);
+        //AdjacentListGraph.insertEdge(adj, 5, 6);
         System.out.println(adj.toString());
         System.out.println(" ");
 
         boolean [] visited = new boolean[v+1];
+        boolean [] recursionStack = new boolean[v+1];
 
         for(int i=0; i<v; i++) {
             if(visited[i] == false) {
-                if (detectCycle(adj, i, visited, -1)) {
+                if (detectCycle(adj, visited, recursionStack, i)) {
                     System.out.println("Cycle detected");
                     return;
                 }
