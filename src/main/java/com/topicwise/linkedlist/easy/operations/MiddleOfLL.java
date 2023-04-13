23fa09954fe75a7d956618ba134f7ab95f3cc8b6
ashwini-s-kumar <ashwiniskumar64@gmail.com>
2023-04-13 09:35:54 +0530

    Linked list

---
+++ src/main/java/com/topicwise/linkedlist/easy/operations/MiddleOfLL.java
@@ -0,0 +1,22 @@
+package com.topicwise.linkedlist.easy.operations;
+
+import com.topicwise.linkedlist.Node;
+
+public class MiddleOfLL {
+    public static void middle(Node head){
+        Node slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println(slow.data);
+    }
+    public static void main(String [] ar){
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(5);
+
+        middle(head);
+    }
+}
